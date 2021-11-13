83ef6334145998a9a7da7ac4a82482a47196a26f
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-13 13:09:00 +0530

    compare To method to find the maximum float number

--- src/GenericsTestMaximum/FindMaximumNumber.java
+++ src/GenericsTestMaximum/FindMaximumNumber.java
@@ -1,21 +1,36 @@
 package GenericsTestMaximum;
-
+import java.lang.Integer;
 import org.junit.Test;
 
 public class FindMaximumNumber {
+
+	public static void main(String[] args) {
+		FindMaxIntegerNumber rv = new FindMaxIntegerNumber();
+		rv.maxNumberatFirstPosition();
+		rv.maxNumberatSecondPosition();
+		rv.maxNumberatThirdPosition();
+		System.out.println("*************************************");
+		FindMaxFloatNumber rv1 = new FindMaxFloatNumber();
+		rv1.maxNumberatFirstPosition();
+		rv1.maxNumberatSecondPosition();
+		rv1.maxNumberatThirdPosition();
+	}
+}
+
+class FindMaxIntegerNumber {
 	@Test
 	int maxNumberatFirstPosition() {
 		Integer num1 = new Integer(34);
 		Integer num2 = new Integer(21);
 		Integer num3 = new Integer(13);
 		if (num1 >= num3 && num1 >= num3) {
-			System.out.println(num1+ " is the maximum number");
+			System.out.println(num1 + " is the maximum number");
 		} else if (num2 >= num1 && num2 >= num3) {
 			System.out.println(num2 + " is the maximum number");
 		} else {
 			System.out.println(num3 + " is the maximum number");
 		}
-		return num1 ;
+		return num1;
 	}
 
 	@Test
@@ -47,14 +62,51 @@ public class FindMaximumNumber {
 		}
 		return num3;
 	}
-	public static void main(String[] args) {
-		FindMaximumNumber rv = new FindMaximumNumber();
-		rv.maxNumberatFirstPosition();
-		rv.maxNumberatSecondPosition();
-		rv.maxNumberatThirdPosition();
-	}
 }
 
+class FindMaxFloatNumber {
+	@Test
+	Float maxNumberatFirstPosition() {
+		Float num1 = new Float(3.4f);
+		Float num2 = new Float(2.1f);
+		Float num3 = new Float(1.3f);
+		if (num1 >= num3 && num1 >= num3) {
+			System.out.println(num1 + " is the maximum float number");
+		} else if (num2 >= num1 && num2 >= num3) {
+			System.out.println(num2 + " is the maximum float number");
+		} else {
+			System.out.println(num3 + " is the maximum float number");
+		}
+		return num1;
+	}
 
-	
+	@Test
+	Float maxNumberatSecondPosition() {
+		Float num1 = new Float(1.6f);
+		Float num2 = new Float(6.9f);
+		Float num3 = new Float(3.5f);
+		if (num1 >= num3 && num1 >= num3) {
+			System.out.println(num1 + " is the maximum float number");
+		} else if (num2 >= num1 && num2 >= num3) {
+			System.out.println(num2 + " is the maximum float number");
+		} else {
+			System.out.println(num3 + " is the maximum float number");
+		}
+		return num2;
+	}
 
+	@Test
+	Float maxNumberatThirdPosition() {
+		Float num1 = new Float(2.1f);
+		Float num2 = new Float(3.7f);
+		Float num3 = new Float(7.3f);
+		if (num1 >= num3 && num1 >= num3) {
+			System.out.println(num1 + " is the maximum float number");
+		} else if (num2 >= num1 && num2 >= num3) {
+			System.out.println(num2 + " is the maximum float number");
+		} else {
+			System.out.println(num3 + " is the maximum float number");
+		}
+		return num3;
+	}
+}
